4cfae231ee4fb0c0933110ff75dfa6e61a25c70d
Fraser Tweedale <frase@frase.id.au>
2016-01-06 15:16:24 +1000

    deploy.sh: fix commit when there are only new files

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,7 @@ export GIT_INDEX_FILE=$PWD/.git/index-deploy
 export GIT_WORK_TREE=$PWD/_site
 REF=refs/heads/gh-pages
 git read-tree "$REF"
+git add --all --intent-to-add
 git diff --quiet && exit
 git add --all
 TREE=$(git write-tree)
